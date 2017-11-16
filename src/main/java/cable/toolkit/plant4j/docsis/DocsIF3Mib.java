7974d33d321df53a9fcbf3cd1101d6c6d09e79cf
Patrick Sandiford <spsandiford@gmail.com>
2017-11-16 11:21:43 -0500

    Adding the rest of the entries in the docsIf3CmtsCmRegStatusTable

--- src/main/java/cable/toolkit/plant4j/docsis/DocsIF3Mib.java
+++ src/main/java/cable/toolkit/plant4j/docsis/DocsIF3Mib.java
@@ -143,5 +143,206 @@ public class DocsIF3Mib {
 	 */
 	public static final OID oid_docsIf3CmtsCmRegStatusValue   = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.6");
 
+	/*
+	 * CmtsCmRegState ::= TEXTUAL-CONVENTION
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This data type defines the CM connectivity states as reported
+	 *         by the CMTS.
+	 *         The enumerated values associated with the CmtsCmRegState are:
+	 * 
+	 *         'other'
+	 *          indicates any state not described below.
+	 *         'initialRanging'
+	 *          indicates that the CMTS has received an Initial Ranging
+	 *          Request message from the CM, and the ranging process is not yet
+	 *          complete.
+	 *         'rangingAutoAdjComplete'
+	 *          indicates that the CM has completed initial ranging and the
+	 *          CMTS sends  a Ranging Status of success in the RNG-RSP.
+	 *         'startEae'
+	 *          indicates that the CMTS has received an Auth Info message for
+	 *          EAE from the CM.
+	 *         'startDhcpv4'
+	 *          indicates that the CMTS has received a DHCPv4 DISCOVER message
+	 *          from the CM.
+	 *         'startDhcpv6'
+	 *          indicates that the CMTS has received a DHCPv6 Solicit message
+	 *          from the CM.
+	 *         'dhcpv4Complete'
+	 *          indicates that the CMTS has sent a DHCPv4 ACK message to the
+	 *          CM.
+	 *         'dhcpv6Complete'
+	 *          indicates that the CMTS has sent a DHCPv6 Reply message to the
+	 *          CM.
+	 *         'startConfigFileDownload'
+	 *          indicates that the CM has started the config file download.
+	 *          If the TFTP Proxy feature is not enabled, the CMTS may not
+	 *          report this state.
+	 *         'configFileDownloadComplete'
+	 *          indicates that the CM has completed the config file download
+	 *          process.  If the TFTP Proxy feature is not enabled, the CMTS
+	 *          is not required to report this state.
+	 *         'startRegistration'
+	 *          indicates that the CMTS has received a Registration
+	 *          Request (REG-REQ or REG-REQ-MP) from the CM.
+	 *         'registrationComplete'
+	 *          indicates that the CMTS has received a Registration Acknowledge
+	 *          (REG-ACK) with a confirmation code of okay/success.
+	 *         'operational'
+	 *          indicates that the CM has completed all necessary
+	 *          initialization steps and is operational.
+	 *         'bpiInit'
+	 *          indicates that the CMTS has received an Auth Info or Auth
+	 *          Request message as part of BPI Initialization.
+	 *         'forwardingDisabled'
+	 *          indicates that the registration process was completed, but 
+	 *          the network access option in the received configuration 
+	 *          file prohibits forwarding.
+	 *         'rfMuteAll'
+	 *          indicates that the CM is instructed to mute all channels 
+	 *          in the CM-CTRL-REQ message from CMTS."
+	 *      REFERENCE
+	 *          "DOCSIS 3.0 MAC and Upper Layer Protocols Interface
+	 *          Specification CM-SP-MULPIv3.0-I08-080522, Cable Modem -
+	 *          CMTS Interaction section."
+	 */
+	public static final int CmtsCmRegState_other = 1;
+	public static final int CmtsCmRegState_initialRanging = 2;
+	public static final int CmtsCmRegState_rangingAutoAdjComplete = 4;
+	public static final int CmtsCmRegState_startEae = 10;
+	public static final int CmtsCmRegState_startDhcpv4 = 11;
+	public static final int CmtsCmRegState_startDhcpv6 = 12;
+	public static final int CmtsCmRegState_dhcpv4Complete = 5;
+	public static final int CmtsCmRegState_dhcpv6Complete = 13;
+	public static final int CmtsCmRegState_startConfigFileDownload = 14;
+	public static final int CmtsCmRegState_configFileDownloadComplete = 15;
+	public static final int CmtsCmRegState_startRegistration = 16;
+	public static final int CmtsCmRegState_registrationComplete = 6;
+	public static final int CmtsCmRegState_operational = 8;
+	public static final int CmtsCmRegState_bpiInit = 9;
+	public static final int CmtsCmRegState_forwardingDisabled = 17;
+	public static final int CmtsCmRegState_rfMuteAll = 18;
 
+	/**
+	 * docsIf3CmtsCmRegStatusMdIfIndex OBJECT-TYPE
+	 *      SYNTAX      InterfaceIndexOrZero
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the interface Index of
+	 *         the CMTS MAC Domain where the CM is active. If the interface
+	 *         is unknown, the CMTS returns a value of zero."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 7 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusMdIfIndex = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.7");
+
+	/**
+	 * docsIf3CmtsCmRegStatusMdCmSgId OBJECT-TYPE
+	 *      SYNTAX      Unsigned32
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the ID of the MAC Domain
+	 *         CM Service Group Id (MD-CM-SG-ID) in which the CM is registered.
+	 *         If the ID is unknown, the CMTS returns a value
+	 *         of zero."
+	 *      REFERENCE
+	 *          "DOCSIS 3.0 MAC and Upper Layer Protocols Interface
+	 *          Specification CM-SP-MULPIv3.0-I08-080522, Cable Modem
+	 *          Service Group (CM-SG) section."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 8 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusMdCmSgId = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.8");
+
+	/**
+	 * docsIf3CmtsCmRegStatusRcpId OBJECT-TYPE
+	 *      SYNTAX      RcpId
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the RCP-ID associated
+	 *         with the CM. If the RCP-ID is unknown the CMTS returns
+	 *         a five octet long string of zeros."
+	 *      REFERENCE
+	 *          "DOCSIS 3.0 MAC and Upper Layer Protocols Interface
+	 *          Specification CM-SP-MULPIv3.0-I08-080522, RCP-ID
+	 *          section in the Common Radio Frequency Interface
+	 *          Encodings Annex."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 9 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusRcpId = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.9");
+
+	/**
+	 * docsIf3CmtsCmRegStatusRccStatusId OBJECT-TYPE
+	 *      SYNTAX      Unsigned32
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the RCC Id the CMTS used
+	 *         to configure the CM receive channel set during the registration
+	 *         process. If unknown, the CMTS returns the
+	 *         value zero."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 10 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusRccStatusId = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.10");
+
+	/**
+	 * docsIf3CmtsCmRegStatusRcsId OBJECT-TYPE
+	 *      SYNTAX      ChSetId
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the Receive Channel Set
+	 *         (RCS) that the CM is currently using. If the RCS is unknown,
+	 *         the CMTS returns the value zero."
+	 *      REFERENCE
+	 *          "DOCSIS 3.0 MAC and Upper Layer Protocols Interface
+	 *          Specification CM-SP-MULPIv3.0-I08-080522, Cable Modem
+	 *          Physical Receive Channel Configuration section and the
+	 *          Receive Channels section in the Common Radio Frequency
+	 *          Interface Encodings Annex."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 11 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusRcsId = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.11");
+
+	/**
+	 * docsIf3CmtsCmRegStatusTcsId OBJECT-TYPE
+	 *      SYNTAX      ChSetId
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents Transmit Channel Set (TCS)
+	 *         the CM is currently using. If the TCS is unknown,
+	 *         the CMTS returns the value zero."
+	 *      REFERENCE
+	 *          "DOCSIS 3.0 MAC and Upper Layer Protocols Interface
+	 *          Specification CM-SP-MULPIv3.0-I08-080522, Changes to
+	 *          the Transmit Channel Set section."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 12 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusTcsId = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.12");
+
+	/**
+	 * docsIf3CmtsCmRegStatusQosVersion OBJECT-TYPE
+	 *      SYNTAX      DocsisQosVersion
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute denotes the queuing services the CM
+	 *         registered, either DOCSIS 1.1 QoS or DOCSIS 1.0 CoS mode."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 13 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusQosVersion = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.13");
+
+	/**
+	 * docsIf3CmtsCmRegStatusLastRegTime OBJECT-TYPE
+	 *      SYNTAX      DateAndTime
+	 *      MAX-ACCESS  read-only
+	 *      STATUS      current
+	 *      DESCRIPTION
+	 *         "This attribute represents the last time the CM registered."
+	 *      ::= { docsIf3CmtsCmRegStatusEntry 14 }
+	 */
+	public static final OID oid_docsIf3CmtsCmRegStatusLastRegTime = new OID("1.3.6.1.4.1.4491.2.1.20.1.3.1.14");
 }
